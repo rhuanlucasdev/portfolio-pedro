9616318b4c380e2d867b57de09e833f8cfb7b5cd
Rhuan Lucas <rhuanlucassbs@gmail.com>
2026-07-03 16:26:48 -0300

    feat: add start menu power actions

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,24 @@ import { WindowProvider } from "./context/WindowContext.jsx";
 
 export default function App() {
   const [booted, setBooted] = useState(false);
+  const [sessionKey, setSessionKey] = useState(0);
+
+  function handlePowerAction(action) {
+    setBooted(false);
+    setSessionKey((key) => key + 1);
+
+    if (action === "restart") {
+      window.setTimeout(() => setBooted(true), 900);
+    }
+  }
 
   return (
-    <WindowProvider>
+    <WindowProvider key={sessionKey}>
       <AnimatePresence mode="wait">
         {!booted ? (
           <BootScreen key="boot" onEnter={() => setBooted(true)} />
         ) : (
-          <Desktop key="desktop" />
+          <Desktop key="desktop" onPowerAction={handlePowerAction} />
         )}
       </AnimatePresence>
     </WindowProvider>
